3826ca85253729ee6f6d4400587d979b64f64262
Asahi <151765581+Satsushi-Takanada@users.noreply.github.com>
2024-03-23 19:15:15 +0800

    Create Character_Selection.h

---
+++ Character_Selection.h
@@ -0,0 +1,26 @@
+#include "Header_Name_Calling.h";
+
+class Character_Type {
+	std::string CharacterName, CharacterChosen;
+	std::string Type[4] = {"WARRIOR", "MAGE", "ROGUE", "ARCHER"};
+
+public:
+	std::string setCharacterName() {
+		std::cout << "\nWhats is your name Adventurer? \n";
+		std::getline(std::cin >> std::ws, CharacterName);
+
+		return CharacterName;
+	}
+
+	std::string setCharacterType() {
+		do {
+			std::cout << "Character Type: \n Warrior\n Mage\n Rogue\n Archer\n\n";
+			std::cout << "Choose Character: ";
+			std::getline(std::cin >> std::ws, CharacterChosen);
+			transform(CharacterChosen.begin(), CharacterChosen.end(), CharacterChosen.begin(), toupper);
+
+		} while (find(begin(Type), end(Type), CharacterChosen) == end(Type));
+
+		return CharacterChosen;
+	}
+};

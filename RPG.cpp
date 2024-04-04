0cdfe825ee8e28e3ba218f31bbacabb95ea083bc
Asahi <151765581+Satsushi-Takanada@users.noreply.github.com>
2024-04-05 00:19:50 +0800

    RPG.cpp

--- RPG.cpp
+++ RPG.cpp
@@ -5,8 +5,11 @@
 int main() {
 	Story RPG;
 	Character_Type User_Character;
+	Player_Stats Player;
 
 	std::string Character_Name, Character_Chosen;
+	int Player_Level = 1;
+	int BattleEXP = 0;
 
 	std::vector<std::string>::iterator User_Character_Chosen;
 
@@ -39,5 +42,19 @@ int main() {
 	RPG.Narration_Story_Begin(Character_Name, Character_Chosen);
 
 	//Main Town
+	do {
+		system("CLS");
+
+		//Get level and stats
+		Player_Level = Player.CPlayer_Level(Player_Level);
+		Player.Class(Character_Chosen);
+
+		//Get EXP
+		Player.PExperience(BattleEXP);
+
+
+		Player.Display(Character_Name, Character_Chosen);
+		std::cin.get();
+	} while (true);
 }
 
